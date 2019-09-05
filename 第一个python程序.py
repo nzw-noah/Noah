f0b875f386770356515a2352ca643c3e4974378d
nzw-noah <54925826+nzw-noah@users.noreply.github.com>
2019-09-05 12:03:57 +0800

    Add files via upload

---
+++ 第一个python程序.py
@@ -0,0 +1,28 @@
+
+# 引号里面为字符、字符串
+print ('hello,word')
+print ('the quick brown fox','jumps over','the lazy dog')
+print ('100+200=',100+200)
+# name为变量，给变量赋值
+name = input ('请输入你的名字：') 
+print ('hello',name)
+
+
+
+# 已知正方形的边长，计算正方形的面积
+# 第一种方法
+a = int (input ('正方形边长a='))
+S = a * a
+print ('该正方形的面积S=',S)
+# 第二种方法
+a = input ('正方形边长a=')
+S = int(a) * int (a)
+print ('该正方形的面积S=',S)
+# 第三种方法
+a = int (input ('正方形边长a='))
+print ('该正方形的面积S=',a*a)
+
+
+
+print ("1024*768=",1024*768)
+
